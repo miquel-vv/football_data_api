86daaf7cd525aad416c972ed2fc42ba1863e874f
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2018-09-01 20:58:08 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="football_data_api",
+    version="0.0.1",
+    author="Miquel Vande Velde",
+    author_email="miquel.vandevelde@gmail.com",
+    description="easy python interface for the football-data.org api from daniel.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
