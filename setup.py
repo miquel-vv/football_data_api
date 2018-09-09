f37e0a0e8c51705d3d19bc869a1e120abc929375
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2018-09-09 18:29:09 +0100

    update install_requires attribute

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="football_data_api",
-    version="0.0.3",
+    version="0.0.6",
     author="Miquel Vande Velde",
     author_email="miquel.vandevelde@gmail.com",
     url='https://github.com/miquel-vv/football_data_api',
-    download_url='https://github.com/miquel-vv/football_data_api/archive/v_03.tar.gz',
     keywords=['football', 'data', 'football-data.org'],
     description="Python interface for the football-data.org api from Daniel.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=['requests'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
